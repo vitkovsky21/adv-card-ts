f3bfe4e4d53394ff4361dd3b3c9f1270de7da856
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2023-03-04 16:15:04 +0300

    everything works well

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,10 +1,13 @@
 import "../App.css";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import api from "../http-common";
+
+let randomNumberArr: any[] = [];
 
 function Cards() {
   const [cardData, setCardData] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
+  const [visited, setVisited] = useState<any>([]);
 
   useEffect(() => {
     api.get("/products").then((res) => {
@@ -12,43 +15,60 @@ function Cards() {
     });
   }, []);
 
+  function visitCard(id: number) {
+    if (!visited) {
+      setVisited([id])
+    } else {
+      setVisited((oldVisited: any) => [...oldVisited, id])
+    }
+  }
+
   if (!cardData) {
     return <div>Loading</div>;
   }
 
+  for (let i = 0; i < cardData.length; i++) {
+    randomNumberArr.push(cardData[i].id);
+  }
+
   const cards = cardData.map((card: any) => {
-    let randomNumber = Math.floor(Math.random() * 100);
+    // let randomNumber = Math.floor(Math.random() * 100);
     let date = new Date(card.date * 1000);
 
     return (
-      <div className="card" key={card.id}>
+      <div onClick={() => visitCard(card.id)} style={{ background: !visited.includes(card.id) ? "#FFF" : "#FFF6A5" }} className="card" key={card.id}>
         <div className="header">
           <div
             style={{ display: loading ? "block" : "none" }}
             className="lds-dual-ring"
-          >
-          </div>
+          ></div>
           <div
             style={{ display: loading ? "none" : "block" }}
             className="header__wallpaper"
           >
             <img
-              src={`https://source.unsplash.com/random?sig=${randomNumber}`}
+              src={`https://source.unsplash.com/random?sig=${randomNumberArr[card.id]}`}
               alt="#"
               onLoad={() => setLoading(false)}
             />
           </div>
 
           <div className="header__logo">
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <div
+              style={{ display: loading ? "none" : "flex" }}
+              className="charts"
+            ></div>
+            <div
+              style={{ display: loading ? "none" : "flex" }}
+              className="like"
+            ></div>
           </div>
         </div>
         <div className="content">
           <div className="content__first-block">
             <div className="price">
-              <p className="price__old">{card.oldPrice}</p>
-              <p className="price__new">{card.price}</p>
+              <p className="price__old">{card.oldPrice} ₽</p>
+              <p className="price__new">{card.price} ₽</p>
             </div>
             <div className="card-logo">
               <div className="card-logo__dostavka"></div>
@@ -73,6 +93,8 @@ function Cards() {
               })}
             </p>
           </div>
+          
+          <div style={{ display: !visited.includes(card.id) ? "hidden" : "flex" }} className="visited">Просмотрено</div>
         </div>
       </div>
     );
